3e341d40affa842f6876fdd60b463ac30dfb8164
Yakob Eliyo <Yakob.Eliyo@yh.nackademin.se>
2023-09-21 08:56:05 +0200

    added client

---
+++ Uppgift1/Client/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Client
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				BilTest bil = new BilTest
+				{
+					Brand = "Volvo",
+					Price = 55,
+					Color = "Pink"
+				};
+				// Create a TcpClient.
+				// Note, for this client to work you need to have a TcpServer
+				// connected to the same address as specified by the server, port
+				// combination.
+				Int32 port = 1111;
+
+				// Prefer a using declaration to ensure the instance is Disposed later.
+				using TcpClient client = new TcpClient("127.0.0.1", port);
+				string jsonBil = JsonConvert.SerializeObject(bil); 
+
+				// Translate the passed message into ASCII and store it as a Byte array.
+				Byte[] data = System.Text.Encoding.ASCII.GetBytes(jsonBil);
+
+				// Get a client stream for reading and writing.
+				NetworkStream stream = client.GetStream();
+
+				// Send the message to the connected TcpServer.
+				stream.Write(data, 0, data.Length);
+
+				Console.WriteLine("Sent: {0}", jsonBil);
+
+				// Receive the server response.
+
+				// Buffer to store the response bytes.
+				data = new Byte[256];
+
+				// String to store the response ASCII representation.
+				String responseData = String.Empty;
+
+				// Read the first batch of the TcpServer response bytes.
+				Int32 bytes = stream.Read(data, 0, data.Length);
+				responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+				Console.WriteLine("Received: {0}", responseData);
+
+				// Explicit close is not necessary since TcpClient.Dispose() will be
+				// called automatically.
+				// stream.Close();
+				// client.Close();
+			}
+			catch (ArgumentNullException e)
+			{
+				Console.WriteLine("ArgumentNullException: {0}", e);
+			}
+			catch (SocketException e)
+			{
+				Console.WriteLine("SocketException: {0}", e);
+			}
+
+			Console.WriteLine("\n Press Enter to continue...");
+			Console.Read();
+		}
+	}
+
+	public class BilTest
+	{
+        public string Color { get; set; } = null!;
+		public int Price { get; set; }
+		public string Brand { get; set; } = null!;
+    }
+}
---
+++ Uppgift1/Server/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace Server
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			TcpListener server = null;
+			try
+			{
+				// Set the TcpListener on port 13000.
+				Int32 port = 1111;
+				IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+				// TcpListener server = new TcpListener(port);
+				server = new TcpListener(localAddr, port);
+
+				// Start listening for client requests.
+				server.Start();
+
+				// Enter the listening loop.
+				while (true)
+				{
+					Console.Write("Waiting for a connection... ");
+
+					// Perform a blocking call to accept requests.
+					// You could also use server.AcceptSocket() here.
+					using TcpClient client = server.AcceptTcpClient();
+					Console.WriteLine("Connected!");
+
+					string data = null!;
+					Byte[] bytes = new Byte[256];
+					// Get a stream object for reading and writing
+					NetworkStream stream = client.GetStream();
+
+					int i;
+
+					// Loop to receive all the data sent by the client.
+					while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+					{
+						// Translate data bytes to a ASCII string.
+						data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+
+						Console.WriteLine("Received: {0}", data);
+						BilTest received = JsonConvert.DeserializeObject<BilTest>(data)!;
+						
+						Console.WriteLine("Received BilTest Object: Brand = {0}, Color = {1}, Price = {2}",
+									 	received.Brand, received.Color, received.Price);
+
+						string responseMessage = "Received and processed the Shoe object.";
+
+						byte[] msg = System.Text.Encoding.ASCII.GetBytes(responseMessage);
+
+						// Send back a response.
+						stream.Write(msg, 0, msg.Length);
+						Console.WriteLine("Sent: {0}", responseMessage);
+					}
+				}
+			}
+			catch (SocketException e)
+			{
+				Console.WriteLine("SocketException: {0}", e);
+			}
+			finally
+			{
+				server!.Stop();
+			}
+
+			Console.WriteLine("\nHit enter to continue...");
+			Console.Read();
+		}
+	}
+	public class BilTest
+	{
+		public string Color { get; set; } = null!;
+		public int Price { get; set; }
+		public string Brand { get; set; } = null!;
+	}
+}
